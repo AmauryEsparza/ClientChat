092e81359ecea54b2cecb192a8e21f6d3a89774b
Amaury Esparza <amauryesparza@gmail.com>
2014-07-14 05:18:39 -0500

    Acticity donde se realiza la interaccion

--- src/com/example/clientchat/ChatActivity.java
+++ src/com/example/clientchat/ChatActivity.java
@@ -17,11 +17,13 @@ public class ChatActivity extends Activity {
 	Button buttonEnviar;
 	EditText editMensaje;
 	ClientHandler objClient;
+	TextView textViewChat;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.chat_view);
+		textViewChat = (TextView) findViewById(R.id.textViewChatField);
 		editMensaje = (EditText) findViewById(R.id.editTextMensaje);
 		buttonEnviar = (Button) findViewById(R.id.buttonEnviar);
 		objClient = new ClientHandler();		
@@ -30,11 +32,14 @@ public class ChatActivity extends Activity {
 			{
 				
 				try {
-					objClient.enviarMensaje(editMensaje.getText().toString(), "Luis");
+					objClient.enviarMensaje(editMensaje.getText().toString(), "0");
+					textViewChat.setText(objClient.getListaMensajesString()+" ");
+					objClient.recibirMensaje("82a38def-e6b4-4803-ac3d-b37d0c486d85");
 				} catch (JSONException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				
 			}
 		});
 		
