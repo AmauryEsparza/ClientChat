9e0782b45172db05ced39a7332ea30a883546fb4
Amaury Esparza <amauryesparza@gmail.com>
2014-07-14 05:16:42 -0500

    List View para mostrar los diferentes usuarios conectados

--- src/com/example/clientchat/UsersActivity.java
+++ src/com/example/clientchat/UsersActivity.java
@@ -1,29 +1,50 @@
 package com.example.clientchat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.json.JSONException;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.app.Activity;
-public class UsersActivity extends Activity {
-	TextView textViewResponse;
+import android.app.ListActivity;
+import android.content.Intent;
+public class UsersActivity extends ListActivity {
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.user_view);
-		textViewResponse = (TextView) findViewById(R.id.textViewResult);
+		super.onCreate(savedInstanceState);		
 		ClientHandler handler = new ClientHandler();
 		try {
-			
-			handler.listarUsuarios(); //Request
-			//Los datos por get			
-			
+			handler.listarUsuarios(); //Request		
 		} catch (JSONException e) {
 			Log.d("UsersActivity", "JsonException");
 			e.printStackTrace();
 		}
+		ListAdapter adaptador = new SimpleAdapter(
+				UsersActivity.this, handler.getArrayListUsuarios(), R.layout.list_item,
+				new String[] {"usuario", "status"}, new int[]{R.id.user, R.id.status});
+		setListAdapter(adaptador);
+		
+		ListView lv = getListView();
+		lv.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent intent = new Intent(getApplicationContext(), ChatActivity.class );
+				startActivity(intent);
+				
+			}
+		});
+		
 		
 		
 		
