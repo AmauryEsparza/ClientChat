5cc9451e1440ed8667ccbec77aa54009c9f4c2b9
Amaury Esparza <amauryesparza@gmail.com>
2014-07-14 23:15:40 -0500

    Se agrego para obtener los EXTRAS

--- src/com/example/clientchat/ChatActivity.java
+++ src/com/example/clientchat/ChatActivity.java
@@ -6,6 +6,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.ClipData.Item;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,11 +20,16 @@ public class ChatActivity extends Activity {
 	EditText editMensaje;
 	ClientHandler objClient;
 	TextView textViewChat;
+	String destinatario;
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.chat_view);
+		
+		destinatario = getIntent().getExtras().getString("DESTINATARIO_ID");
+		Log.d("CHATACTIVITY DESTINATARIO", destinatario+" ");
+		//String destinatario = getActivity().
 		textViewChat = (TextView) findViewById(R.id.textViewChatField);
 		editMensaje = (EditText) findViewById(R.id.editTextMensaje);
 		buttonEnviar = (Button) findViewById(R.id.buttonEnviar);
@@ -30,11 +37,10 @@ public class ChatActivity extends Activity {
 		buttonEnviar.setOnClickListener(new Button.OnClickListener(){
 			public void onClick(View v)
 			{
-				
 				try {
-					objClient.enviarMensaje(editMensaje.getText().toString(), "0");
+					objClient.enviarMensaje(editMensaje.getText().toString(), destinatario);
 					textViewChat.setText(objClient.getListaMensajesString()+" ");
-					objClient.recibirMensaje("82a38def-e6b4-4803-ac3d-b37d0c486d85");
+					objClient.recibirMensaje(destinatario);
 				} catch (JSONException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
